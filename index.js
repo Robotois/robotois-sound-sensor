3012fb107fd6cf52aac506b8fa5abd8fce40559a
yovany <yovany@gmail.com>
2017-10-24 16:24:40 +0000

    MQTT client

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function SoundSensor(port, add = 0) {
 
   this.sound = new SSensor(port, add);
   this.eventsEnable = false;
+  this.prevValue = 0;
 
   process.on('SIGINT', () => {
     self.sound.release();
@@ -32,7 +33,12 @@ SoundSensor.prototype.getBasicValue = function getBasicValue() {
   return this.sound.getBasicValue();
 };
 
-SoundSensor.prototype.enableEvents = function enableEvents() {
+SoundSensor.prototype.enableEvents = function enableEvents(mqttConfig) {
+  if (mqttConfig) {
+    this.mqttClient = mqttConfig.mqttClient;
+    this.myTopic = `sensors/sound${mqttConfig.instance}`;
+    this.mqttClient.publish('registerTopic', this.myTopic);
+  }
   if (!this.eventsEnable) {
     this.eventsEnable = true;
     this.sampling();
@@ -43,14 +49,26 @@ SoundSensor.prototype.disableEvents = function disableEvents() {
   this.eventsEnable = false;
 };
 
+SoundSensor.prototype.publishNow = function publishNow() {
+  this.mqttClient.publish(this.myTopic, this.sound.getBasicValue().toString());
+};
+
 SoundSensor.prototype.sampling = function sampling() {
   const self = this;
   function run() {
     if (!self.eventsEnable) {
       return;
     }
-    const value = self.sound.getBasicValue();
-    self.emit('medicion', value);
+    const currentValue = self.sound.getBasicValue();
+    let diffValue = self.prevValue - currentValue;
+    diffValue = diffValue < 0 ? diffValue * -1 : diffValue;
+    if (diffValue > 1) {
+      self.emit('medicion', currentValue);
+      if (self.mqttClient) {
+        self.mqttClient.publish(self.myTopic, currentValue.toString());
+      }
+      self.prevValue = currentValue;
+    }
     setImmediate(run);
   }
   run();
